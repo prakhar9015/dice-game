605c56526812d2b0ce1185b5fea82df36d3dcf77
prakhar9015 <104693414+prakhar9015@users.noreply.github.com>
2022-11-08 12:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 
-
 var randomnumber1 = Math.floor(Math.random()*6)+1;
 
 var randomnumber2 = Math.floor(Math.random()*6)+1;
 
-// var random_img_num =  Math.floor(Math.random()*2)+1;
-
 document.querySelector(".img1").src = "images/dice"+randomnumber1+".png";
 
 document.querySelector(".img2").src = "images/dice"+randomnumber2+".png";
-
-
 
 
 if (randomnumber1 == randomnumber2) {
