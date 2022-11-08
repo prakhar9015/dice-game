f22d89835ae320e39b1a56caebeb292cee9de72d
prakhar9015 <104693414+prakhar9015@users.noreply.github.com>
2022-11-08 20:00:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ document.querySelector(".btn").addEventListener("click", btn_click);
 
 
 function btn_click() {
-  
- var randomnumber1 = Math.floor(Math.random()*6)+1;
 
-var randomnumber2 = Math.floor(Math.random()*6)+1;
+  var randomnumber1 = Math.floor(Math.random()*6)+1;
+  var randomnumber2 = Math.floor(Math.random()*6)+1;
 
-document.querySelector(".img1").src = "images/dice"+randomnumber1+".png";
-
-document.querySelector(".img2").src = "images/dice"+randomnumber2+".png";
+  document.querySelector(".img1").src = "images/dice"+randomnumber1+".png";
+  document.querySelector(".img2").src = "images/dice"+randomnumber2+".png";
 
 
-if (randomnumber1 == randomnumber2) {
-  document.querySelector("h1").innerHTML = "Draw";
-}
-else if (randomnumber1 > randomnumber2) {
-  document.querySelector("h1").innerHTML = "🚩Player 1 wins";
-}
-else  {
-  document.querySelector("h1").innerHTML = "Player 2 wins 🚩";
-}
+  if (randomnumber1 == randomnumber2) {
+    document.querySelector("h1").innerHTML = "Draw";
+  }
+  else if (randomnumber1 > randomnumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins";
+  }
+  else  {
+    document.querySelector("h1").innerHTML = "Player 2 wins 🚩";
+  }
 
 }
-
-
-
